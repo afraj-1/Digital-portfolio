1acd3c29309d23f23c95f876461927d4e04d9a50
afraj-1 <afrajuwairiya@gmail.com>
2025-03-21 20:12:52 +0530

    Update and rename scripts.js to script.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function tipcal() {
-    var total=document.getElementById("tba").value;
-    var quality=document.getElementById("qs").value;
-    var ppl=document.getElementById("np").value;
-
-    cal=(total*quality)/ppl;
-    document.getElementById("result").innerHTML=("Calculated tip: Rs."+cal +" per person")
-
-}
